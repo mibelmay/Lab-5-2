c7588802ee40fac6f9df1ab3f7b46f1d1ca9d492
Stacy <zemcovannastasia11@gmail.com>
2022-10-15 15:56:29 +0500

    added method CheckIfInputIsCorrect

--- Lab-5-2.cs
+++ Lab-5-2.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Labs
+{
+    public static class Program
+    { 
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(CheckIfInputIsCorrect(input));
+        }
+
+        public static bool CheckIfInputIsCorrect(string input)
+        {
+            char[] digits = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            bool flag = false;
+            if (input == "") return flag;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == 0 && input[i] == '-')
+                    continue;
+                if (digits.Contains(input[i]))
+                    flag = true;
+                else flag = false;
+            }
+            return flag;
+        }
+    }
+
+}
