c7bc2aff340382bfd74399f1fd9d5076c170b773
Stacy <zemcovannastasia11@gmail.com>
2022-10-15 16:18:36 +0500

    modified

--- Lab-5-2.cs
+++ Lab-5-2.cs
@@ -6,25 +6,31 @@ namespace Labs
     { 
         public static void Main()
         {
+            Console.WriteLine("Введите число:");
             string input = Console.ReadLine();
-            Console.WriteLine(CheckIfInputIsCorrect(input));
+            if (!CheckIfInputIsCorrect(input))
+                Console.WriteLine("Введено некорректное число");
+            else
+                Console.WriteLine("Число корректно");
+
         }
 
         public static bool CheckIfInputIsCorrect(string input)
         {
-            char[] digits = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
             bool flag = false;
-            if (input == "") return flag;
+            if (input == "" || input.Contains(',') || input.Contains('.')) return flag;
             for (int i = 0; i < input.Length; i++)
             {
                 if (i == 0 && input[i] == '-')
                     continue;
-                if (digits.Contains(input[i]))
+                if (new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }.Contains(input[i]))
                     flag = true;
                 else flag = false;
             }
             return flag;
         }
+
+
     }
 
 }
